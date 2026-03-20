0211b662101ec3cec40e823460cbe12220410987
Spyd3r05 <agnesnzivu05@gmail.com>
2026-03-20 11:24:31 +0300

    feat: fetch posts from DB in a server coomponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { prisma } from "./lib/prisma";
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
     <div>
       <h1>Home</h1>
+      {posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <small>{new Date(post.createdAt).toLocaleDateString()}</small>
+            <hr />
+          </div>
+        );
+      })}
     </div>
   );
 }
